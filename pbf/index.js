907ab300c2177ef51e8f582f4e56d69c6bfc051b
Prasundas99 <prasundas115@gmail.com>
2022-06-04 23:52:47 +0530

    JSON application and why to use protobuffers

--- pbf/index.js
+++ pbf/index.js
@@ -1,8 +1,15 @@
 /**
  * Writing an application in JSON and discovering its different features
+ * Steps:
+ * 1. Creating employees array
+ * 2. Adding Data to the array in different format
+ * 3. Adding File System and Storing it in File System
  */
+
+const fs = require('fs');
 const employee = []
 
+// Daaing Data 1
 employee.push({
     name: 'John',
     age: 30,
@@ -11,6 +18,7 @@ employee.push({
     "id": 1
 })
 
+//Adding data by creating an object
 const ahmed = {
     "name": "Ahmed",
     "age": 30,
@@ -21,12 +29,43 @@ const ahmed = {
 
 employee.push(ahmed)
 
-employee.push({
-    name: 'Jit',
-    age: 20,
-    salary: 2000,
-    position: 'Developer',
-    "id": 3
-})
+//Adding data by creating array of objects
+const employees = [
+    {
+        "name": "John",
+        "age": 30,
+        "salary": 1000,
+        "position": "Manager",
+        "id": 4
+    },
+    {
+        "name": "Ahmed",
+        "age": 30,
+        "salary": 1001,
+        "position": "Director",
+        "id": 5
+    }
+]
+//Create an employee array of abjects of 20 datas in it
+for (let i = 6; i < 26; i++) {
+    employee.push({
+        "name": "John"+ i,
+        "age": 3 + i,
+        "salary": 100 + i,
+        "position": "Manager of" + i + "th Department",
+        "id": i
+    })
+}
 
+fs.writeFileSync('employee.json', JSON.stringify(employee))
+/**
+ * Just for 25 Employees it gives 2.8kb file size
+ * Not much file size but imagine it is used in production having lakhas of employees this will grow large and large
+ * So whenever we send this file as http request it will be huge
+ * How about we do it with protobuf?
+ * 
+ * What is protobuff?
+ * It is a representation of structured data, JSON is not a structured data
+ * 
+ */
 console.log(JSON.stringify(employee, null, 2));
